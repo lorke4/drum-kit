3910a978041613be21f5a32d191e68ca8dfe5447
Máté <mate.lorinczi97@gmail.com>
2022-07-28 14:08:26 +0200

    Done project

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-window.addEventListener('keydown', function(e) {
-    console.log(e.code)
-});
+function playSound(e){
+    const audio = document.querySelector(`audio[data-key=${e.code}]`);
+    const key = document.querySelector(`.key[data-key="${e.code}"]`)
+    if(!audio) return; // stop the function from running.
+    audio.currentTime = 0; // rewind to start
+    audio.play();
+    console.log(key);
+    key.classList.add('playing');
+}
+
+function removeTransition(e){
+    if(e.propertyName !== 'transform') return;
+    this.classList.remove('playing')
+}
+
+const keys = document.querySelectorAll('.key')
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
